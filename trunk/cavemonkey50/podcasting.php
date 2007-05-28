90e6de47353485695ea1d8ff72731398083e7d59
cavemonkey50 <cavemonkey50@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-05-28 16:51:31 +0000

    Adding initial podcasting files

---
+++ trunk/cavemonkey50/podcasting.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Podcasting
+Version: 0.01
+Plugin URI: http://code.google.com/p/wordpress-soc-2007/
+Description: Adds full podcasting support.
+Author: Ronald Heft, Jr.
+Author URI: http://cavemonkey50.com/
+*/
+
+?>
---
+++ trunk/cavemonkey50/readme.txt
@@ -0,0 +1,12 @@
+=== Plugin Name ===
+Contributors: cavemonkey50
+Tags: podcast, itunes, podcasting, rss, feed, enclosure
+Requires at least: 2.2
+Tested up to: 2.2
+Stable tag: 0.01
+
+Adds full podcasting support.
+
+== Description ==
+
+
