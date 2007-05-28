791f291a92723e096b1eeaebad60d8a39556a435
cavemonkey50 <cavemonkey50@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-05-28 17:28:58 +0000

    Creates Podcasting option page

--- cavemonkey50/podcasting.php
+++ cavemonkey50/podcasting.php
@@ -9,4 +9,40 @@ Author: Ronald Heft, Jr.
 Author URI: http://cavemonkey50.com/
 */
 
+/* ------------------------------------- SETUP ------------------------------------- */
+
+// Add Podcasting to the options menu
+function add_podcasting_pages() {
+	add_options_page('Podcasting Options', 'Podcasting', 8, basename(__FILE__), 'podcasting_options_page');
+}
+
+add_action('admin_menu', 'add_podcasting_pages');
+
+
+/* ------------------------------------ OPTIONS ------------------------------------ */
+
+// Podcasting options page
+function podcasting_options_page() {
+	?>
+	
+	<form method="post" action="options-general.php?page=podcasting.php">
+	<div class="wrap">
+		<h2>Podcasting Options</h2>
+		
+		<fieldset class="options">
+			<legend>Podcast Details</legend>
+		</fieldset>
+		
+		<fieldset class="options">
+			<legend>iTunes Specifics</legend>
+		</fieldset>
+	</div>
+	</form>
+	
+	<?php
+} // End podcasting_options_page()
+
+
+/* ------------------------------------- WORK -------------------------------------- */
+
 ?>
