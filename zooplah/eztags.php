488fcf4600a3ea25425fb7391c4275e1aed65671
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-06-07 23:45:12 +0000

    OK, it seems to be in the beginning of working

--- zooplah/eztags.php
+++ zooplah/eztags.php
@@ -8,58 +8,97 @@ Author: Keith Bowes
 Author URI: http://zooplah.farvista.net/
 */
 
+/* Global vars (necessary evil unless I can figure out a better way
+ * to do it) */
 $contents = '';
-$template_name = '';
-
-define('TEMPLATEPATH', get_template_directory());;
-define('STYLESHEETPATH', get_stylesheet_directory());
-include ABSPATH . WPINC . '/locale.php';
-$wp_locale =& new WP_Locale();
-$wp_query =& new WP_Query();
-echo $wp_query == NULL;
 
 class EzTags
 {
 	var $_template;
 	var $_template_home_file;
-	function processTemplate()
+
+	/* Initialize everything needed to make the system go.
+	 * Necessary due to the things I'm overriding. */
+	static function initialize()
 	{
-		global $contents, $template_name;
+		require_once ABSPATH . WPINC . '/pluggable.php';
+	}
+
+	/* Get the template's name */
+	function getTemplateName()
+	{
+		static $template_name;
+
 		if ( empty($template_name) )
 			$template_name = get_option('template');
 		else
 			update_option('template', $template_name);
 
+		return $template_name;
+	}
+
+	/* Replace the tags with PHP */
+	function replace(&$contents)
+	{
+		$contents = str_replace('<$Loop$>', '<?php echo "Hello World<br>\n"; ?>', $contents);
+	}
+
+	/* Just process the template and fill the content */
+	function processTemplate()
+	{
+		global $contents, $wp_query;
+
+		$template_name = EzTags::getTemplateName();
+
 		if ( is_admin() ) return $template_name;
 
 		$_template = ABSPATH . 'wp-content/themes/' . $template_name;
 
-		if (file_exists($_template . '/index.php')) $_template_home_file = $_template . '/index.php';
-		else if (file_exists($_template . '/home.php')) $_template_home_file = $_template . '/home.php';
+		if ( file_exists($_template . '/index.php') )
+			$_template_home_file = $_template . '/index.php';
+		else if ( file_exists($_template . '/home.php') )
+			$_template_home_file = $_template . '/home.php';
 
 		$contents = file_get_contents($_template_home_file);
 		$contents = preg_replace('/^(.*)<\?(php)?/', '$1', $contents);
-		//eval($contents);
+		EzTags::replace($contents);
+		eval($contents);
 
-		return null;
+		return NULL;
 	}
-	function getStylesheet($a)
+
+	/* Ah, small function needed so that we get the right style sheet.
+	 * Shouldn't be necessary, I know.  But it is. */
+	function getStylesheet()
 	{
-		global $template_name;
+		$template_name = EzTags::getTemplateName();
 		return $template_name;
 	}
-	function outputParsed($a)
+
+	/* Get the title */
+	function getTitle($a,$b,$c)
 	{
-		global $contents;
-		if ( $contents !== '' )
-			eval($contents);
-		return true;
+		echo 'Getting title...';
+		$title = get_the_title();
+		echo strlen($title);
+
+		if ( empty($title) )
+		{
+			$id = 0;
+			$post = &get_post($id);
+			var_dump($post);
+			$title = $post->post_title;
+		}
+
+		return $title;
+
 	}
 }
 
+EzTags::initialize();
 
 add_filter('template', array(EzTags, 'processTemplate'));
 add_filter('stylesheet', array(EzTags, 'getStylesheet'));
-add_action('loop_begin', array(EzTags, 'outputParsed'));
+add_filter('the_title', array(EzTags, 'getTitle'), 3);
 
 ?>
