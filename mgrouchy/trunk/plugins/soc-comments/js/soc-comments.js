5d0cbd7614a43d01ee1975a8af24e650d3d48aee
mgrouchy <mgrouchy@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-06-25 15:29:21 +0000

    fixing some js foolishness, was embedding php using innerhtml, that obviously doesnt work

--- mgrouchy/trunk/plugins/soc-comments/js/soc-comments.js
+++ mgrouchy/trunk/plugins/soc-comments/js/soc-comments.js
@@ -1,22 +1,16 @@
-function addReplyForm(commentid){
+function addReplyForm(commentid,userident,useremail,userurl){
 	var divname = 'com-' + commentid;
 	var divid = '#' + divname;
 	jQuery('#' + divname).css({display: 'none'});
-	jQuery("<?php if ('open' == $post->comment_status) : ?>").appendTo(divid);
 	jQuery("<form action=\"<?php echo get_option('siteurl'); ?>/wp-comments-post.php\" method=\"POST\" id=\"comment-reply-form\">").appendTo(divid);
 	jQuery("<p><textarea name=\"comment\" id=\"comment\" cols=\"100%\" rows=\"10\" tabindex=\"4\"></textarea></p>").appendTo(divid);
 	jQuery("<p><input name=\"submit\" type=\"submit\" id=\"submit\" tabindex=\"5\" value=\"Submit Comment\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" name=\"comment_post_ID\" value=\"<?php echo $id; ?>\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" id=\"author\" name=\"author\" value=\"<?php echo $user_identity; ?>\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" id=\"email\" name=\"email\" value=\"<?php echo $user_email; ?>\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" id=\"url\" name=\"url\" value=\"<?php echo $user_url; ?>\" />").appendTo(divid);
+	jQuery("<input type=\"hidden\" name=\"comment_post_ID\" value=\"" + commentid + "\" />").appendTo(divid);
+	jQuery("<input type=\"hidden\" id=\"author\" name=\"author\" value=\"" + userident + "\" />").appendTo(divid);
+	jQuery("<input type=\"hidden\" id=\"email\" name=\"email\" value=\"" + useremail + "\" />").appendTo(divid);
+	jQuery("<input type=\"hidden\" id=\"url\" name=\"url\" value=\""+ userurl + "\" />").appendTo(divid);
 	jQuery("</p>").appendTo(divid);
-	jQuery("<?php do_action('comment_form', $post->ID); ?>").appendTo(divid);
 	jQuery("</form>").appendTo(divid);
-	jQuery("<?php else : ?>").appendTo(divid);
-	jQuery("<p> Sorry. Comments are closed for this post").appendTo(divid);
-	jQuery("<?php endif; ?>").appendTo(divid);
-	
 	bindForm(divname,'comment-reply-form');
 	
 }
