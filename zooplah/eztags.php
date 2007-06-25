e204b24a2871b7b9ae5f389983252885a57abd2e
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-06-25 18:51:43 +0000

    Started working on The New Way (tm)

--- zooplah/eztags.php
+++ zooplah/eztags.php
@@ -8,78 +8,12 @@ Author: Keith Bowes
 Author URI: http://zooplah.farvista.net/
 */
 
-require_once 'eztagstags.php';
+require_once ABSPATH . WPINC . '/pluggable.php';
 
-/* The main class for Easier Template Tags */
-class EzTags
-{
-	var $_template;
-	var $_template_home_file;
+require_once ABSPATH . '/wp-admin/admin-functions.php';
+require_once ABSPATH . '/wp-admin/admin.php';
+require_once ABSPATH . '/wp-admin/menu.php';
 
-	/* Initialize everything needed to make the system go.
-	 * Necessary due to the things I'm overriding. */
-	static function initialize()
-	{
-		require_once ABSPATH . WPINC . '/pluggable.php';
-	}
-
-	/* Get the template's name */
-	function getTemplateName()
-	{
-		static $template_name;
-
-		if ( empty($template_name) )
-			$template_name = get_option('template');
-		else
-			update_option('template', $template_name);
-
-		return $template_name;
-	}
-
-	/* Replace the tags with PHP */
-	function replace(&$contents)
-	{
-		global $_eztags_entries_loop_begin;
-		echo $_eztags_entries_loop_begin;
-
-		$contents = str_replace('<Entries>',  '<?php global $wp_query; var_dump($wp_query); ?>', $contents);
-	}
-
-	/* Just process the template and fill the content */
-	function processTemplate()
-	{
-		global $contents;
-		if ( $contents != '' ) return;
-
-		$template_name = EzTags::getTemplateName();
-
-		if ( preg_replace('/^(.*)\/$/', '$1', 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) != get_option('siteurl') ) return $template_name;
-
-		$_template = ABSPATH . 'wp-content/themes/' . $template_name;
-
-		if ( file_exists($_template . '/index.php') )
-			$_template_home_file = $_template . '/index.php';
-		else if ( file_exists($_template . '/home.php') )
-			$_template_home_file = $_template . '/home.php';
-
-		$contents = file_get_contents($_template_home_file);
-		$contents = preg_replace('/^(.*)<\?(php)?/', '$1', $contents);
-		EzTags::replace($contents);
-		eval($contents);
-
-		return NULL;
-	}
-
-	/* Ah, small function needed so that we get the right style sheet.
-	 * Shouldn't be necessary, I know.  But it is. */
-	function getStylesheet()
-	{
-		$template_name = EzTags::getTemplateName();
-		return $template_name;
-	}
-}
-
-add_filter('template', array(EzTags, 'processTemplate'));
-add_filter('stylesheet', array(EzTags, 'getStylesheet'));
+add_theme_page('Foobar', 'Foobarspiel', edit_themes, 'eztagspanel.php');
 
 ?>
