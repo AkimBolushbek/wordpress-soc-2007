bfc4f6d12257581d5076dcfb8a0896652fb2918d
mgrouchy <mgrouchy@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-06-28 16:04:04 +0000

    fixed form creation and bindform function

--- mgrouchy/trunk/plugins/soc-comments/js/soc-comments.js
+++ mgrouchy/trunk/plugins/soc-comments/js/soc-comments.js
@@ -1,24 +1,21 @@
-function addReplyForm(commentid){
+function addReplyForm( commentid, userident, useremail, userurl){
 	var divname = 'com-' + commentid;
 	var divid = '#' + divname;
+	// build our form and put it the comments div
 	jQuery('#' + divname).css({display: 'none'});
-	jQuery("<?php if ('open' == $post->comment_status) : ?>").appendTo(divid);
-	jQuery("<form action=\"<?php echo get_option('siteurl'); ?>/wp-comments-post.php\" method=\"POST\" id=\"comment-reply-form\">").appendTo(divid);
-	jQuery("<p><textarea name=\"comment\" id=\"comment\" cols=\"100%\" rows=\"10\" tabindex=\"4\"></textarea></p>").appendTo(divid);
-	jQuery("<p><input name=\"submit\" type=\"submit\" id=\"submit\" tabindex=\"5\" value=\"Submit Comment\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" name=\"comment_post_ID\" value=\"<?php echo $id; ?>\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" id=\"author\" name=\"author\" value=\"<?php echo $user_identity; ?>\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" id=\"email\" name=\"email\" value=\"<?php echo $user_email; ?>\" />").appendTo(divid);
-	jQuery("<input type=\"hidden\" id=\"url\" name=\"url\" value=\"<?php echo $user_url; ?>\" />").appendTo(divid);
-	jQuery("</p>").appendTo(divid);
-	jQuery("<?php do_action('comment_form', $post->ID); ?>").appendTo(divid);
-	jQuery("</form>").appendTo(divid);
-	jQuery("<?php else : ?>").appendTo(divid);
-	jQuery("<p> Sorry. Comments are closed for this post").appendTo(divid);
-	jQuery("<?php endif; ?>").appendTo(divid);
+	jQuery("<form action=\"/wp-comments-post.php\" method=\"POST\" id=\"comment-reply-form\">" + 
+	      "<p><textarea name=\"comment\" id=\"comment\" cols=\"100%\" rows=\"10\" tabindex=\"4\"></textarea></p>" +
+	      "<p><input name=\"submit\" type=\"submit\" id=\"submit\" tabindex=\"5\" value=\"Submit Comment\" />" +
+	      "<input type=\"hidden\" name=\"comment_post_ID\" value=\"" + commentid + "\" />" +
+	      "<input type=\"hidden\" id=\"author\" name=\"author\" value=\"" + userident + "\" />" +
+	      "<input type=\"hidden\" id=\"email\" name=\"email\" value=\"" + useremail + "\" />" +
+	      "<input type=\"hidden\" id=\"url\" name=\"url\" value=\"" + userurl + "\" />" +
+	      "</p></form>").appendTo(divid);
 	
 	bindForm(divname,'comment-reply-form');
+	jQuery(divid).show("slow");
 	
+	return false;	
 }
 
 function bindForm(divname,formname) {
@@ -28,7 +25,7 @@ function bindForm(divname,formname) {
 	        beforeSubmit: showRequest,
 	        success: showResponse,
 	    };
-	// bind 'myForm' and provide a simple callback function
+	// bind form and provide a simple callback function
 	    jQuery('#' + formname).ajaxForm(options);
 	});
 }
@@ -37,17 +34,12 @@ function showRequest(formData, jqForm, options) {
 
     var qs = jQuery.param(formData);
     alert(qs);
-    return true;
+    return true; 
+	
 }
 
 
 function showResponse(responseText, statusText) {
-	if (statusText == "success" ){
 		alert('responseText: \n' + responseText );
-
-	}
+		}
 }
-
-
-
-
