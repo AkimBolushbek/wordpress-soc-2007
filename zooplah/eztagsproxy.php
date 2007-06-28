b662c3d8f76635c84c7c2f17e5b8cd52959efceb
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-06-28 23:31:07 +0000

    New version name; better split of files; better submenu/tab name in the admin area

--- zooplah/eztagsproxy.php
+++ zooplah/eztagsproxy.php
@@ -2,31 +2,8 @@
 
 /* Converts back and forth between the two types of template tags */
 
-/* Strips PHP from the file */
-function stripPHP(&$content)
-{
-	/* Preserve the special tags */
-	function preserve(&$content)
-	{
-		$content = preg_replace('/&lt;\?php\s*\/\/\s*!!!/', '<!php', $content);
-		$content = preg_replace('/\/\/\s*!!!\s*\?&gt;/', '!>', $content);
-	}
-
-	/* And restore 'em */
-	function restore(&$content)
-	{
-		$content = str_replace('<!php', '<?php', $content);
-		$content = str_replace('!>', '?>', $content);
-	}
-
-	preserve($content);
-
-	/* Get rid of the PHP start and end markers */
-	$content = preg_replace('/&lt;\?php\s*/', '', $content);
-	$content = preg_replace('/\s*\?&gt;/', '', $content);
-
-	restore($content);
-}
+require_once 'eztags_fromstandard.php';
+require_once 'eztags_tostandard.php';
 
 /* Replace Standard tags with Easy tags */
 function std2ez($content)
@@ -37,16 +14,6 @@ function std2ez($content)
 	$content = str_replace('the_title();', '<$EntryTitle$>', $content);
 
 	return $content;
-}
-
-/* Insert the markers indicating to-be-presevered PHP back in */
-/* This would be so much easier in Esperanto.
- * I could just say enmetuKonservilojn
- * And additonally, you guys wouldn't be accusing me of making up words */
-function insertPreservers(&$content)
-{
-	$content = str_replace('<?php', '<?php //!!!', $content);
-	$content = str_replace('?>', '//!!! ?>', $content);
 }
 
 /* Replace Easy tags with Standard tags */
