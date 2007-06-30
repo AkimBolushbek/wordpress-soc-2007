0a38e61f55206e5d10e37f30b7079f5a5becd157
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-06-30 00:00:39 +0000

    Did a few hours more work on the prototype

--- trunk/zooplah/eztagsproxy.php
+++ trunk/zooplah/eztagsproxy.php
@@ -11,7 +11,19 @@ function std2ez($content)
 	stripPHP($content);
 
 	/* The Actual tags */
+	$content = preg_replace('/if\s*\(have_posts\(\)\)\s*:\s*while\s*\(have_posts\(\)\)\s*:\s*the_post\(\);/m', '<Entries>', $content);
+	$content = str_replace('endwhile;', "</Entries>", $content);
+	$content = preg_replace('/else\s*:/', '<NoEntries>', $content);
+	$content = str_replace('endif;', '</NoEntries>', $content);
+
+	$content = str_replace('get_header();', '<$Header$>', $content);
+	$Content = str_replace('the_author();', '<$EntryAuthor$>', $content);
+	$content = preg_replace('/the_date\([^\)]+\);/', '<$EntryDate$>', $content);
+	$content = str_replace('the_ID();', '<$EntryID$>', $content);
+	$content = preg_replace('/the_permalink\(\);?/', '<$EntryPermalink$>', $content);
+	$content = str_replace('the_time();', '<$EntryTime$>', $content);
 	$content = str_replace('the_title();', '<$EntryTitle$>', $content);
+	$content = str_replace('wp_link_pages();', '<$LinkPages$>', $content);
 
 	return $content;
 }
@@ -21,7 +33,19 @@ function ez2std($content)
 {
 	insertPreservers($content);
 
+	$content = str_replace('<Entries>' , '<?php if (have_posts()): while (have_posts()): the_post(); ?>', $content);
+	$content = str_replace('</Entries', '<?php endwhile; ?>', $content);
+	$content = str_replace('<NoEntries>', '<?php else: ?>', $content);
+	$content = str_replace('</NoEntries>', '<?php endif; ?>', $content);
+
+	$content = str_replace('<$EntryAuthor', '<?php the_author();', $content);
+	$content = str_replace('<$EntryDate$>', "<?php the_date('', '<h2>', '</h2>')", $content);
+	$content = str_replace('<$EntryId$>', '<?php the_id(); ?>', $content);
+	$content = str_replace('<$EntryPermalink$>', '<?php the_permalink(); ?>', $content);
+	$content = str_replace('<$EntryTime$>', '<?php the_time(); ?>', $content);
 	$content = str_replace('<$EntryTitle$>', '<?php the_title(); ?>', $content);
+	$content = str_replace('<$Header$>', '<?php get_header(); ?>', $content);
+	$content = str_replace('<$LinkPages$>', '<?php wp_link_pages(); ?>', $content);
 
 	return $content;
 }
