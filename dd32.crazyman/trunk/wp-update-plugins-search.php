28cace99866e983fe250dc33c906048c02becc67
dd32.crazyman <dd32.crazyman@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-01 13:29:57 +0000

    Basic Plugin search UI

--- dd32.crazyman/trunk/wp-update-plugins-search.php
+++ dd32.crazyman/trunk/wp-update-plugins-search.php
@@ -4,8 +4,17 @@ if( !get_option('update_plugin_search_enable') ){
 	return;
 }
 require_once('includes/wp-update-class.php');
-global $wpupdate;
+global $wpupdate,$pagenow;
 $wpupdate = new WP_Update;
+
+$url = $pagenow . '?page=' . $_GET['page'];
+
+if( isset($_POST['term']) || isset($_GET['tag']) ){
+	if( isset($_POST['term']))
+		$searchTerm = $_POST['term'];
+	else
+		$searchTerm = $_GET['tag'];
+}
 
 //$pluginInfo = $wpupdate->getPluginInformationWordPressOrg("http://wordpress.org/extend/plugins/google-sitemap-generator-ultimate-tag-warrior-tags-addon/");
 //var_dump($pluginInfo);
@@ -16,53 +25,60 @@ $wpupdate = new WP_Update;
 	.taglist p{
 		font-size:2em;
 	}
+	.pluginitem{
+		text-indent:30px;
+		margin-bottom:0px;
+	}
+	.pluginitem p{
+		background-color:#DEDEDE;
+	}
 	.pluginitem h3{
 		text-decoration:underline;
 		font-weight:bolder;
 		font-size:1.4em;
 	}
+	.section{
+		font-weight:bold;
+		padding-left:50px;
+		margin:0px;
+	}
 </style>
 <div class="wrap">
+	<h2>Search for Plugins<?php echo !empty($searchTerm) ? ': '.$searchTerm : 'via terms'; ?></h2>
+	<div class="tagsearch">
+		<form method="post" action="<?php echo $url; ?>">
+			<input type="text" name="term" value="<?php if( !empty($searchTerm)){ echo attribute_escape($searchTerm);}  ?>" />
+			<input type="submit" name="submit" value="Search" />
+		</form>
 <?php
-	if( !isset($_POST['term']) ){
+	if( ! empty($searchTerm) ){
+		echo '<div id="searchresults">';
+			$results = $wpupdate->searchPlugins($searchTerm);
+			var_dump($results);
+			foreach((array)$results as $section=>$res){
+				if('titlematch' == $section) echo '<h2>Title Matches</h2>';
+				if('relevant' == $section) echo '<h2>Relevant Results</h2>';
+				foreach((array)$res as $plugin){
+					echo '<div class="pluginitem">';
+						echo '<h3>'.$plugin['Name'].'</h3><p>';
+						echo nl2br($plugin['Desc']);
+						echo "</p><div class='section'><a href='#'>Install</a> <a href='{$plugin['Uri']}' target='_blank'>{$plugin['Name']} on WordPress.Org</a></div>\n\n";
+					echo '</div>';
+				} //foreach
+			} //foreach
+		echo '</div>';
+	} //end if empty
 ?>
+	</div>
+
 	<h2>Search for Plugins via Tags</h2>
 	<div class="taglist">
 		<p>
 <?php
 	$tags = $wpupdate->getPluginSearchTags();
 	foreach($tags as $tag)
-		echo "\t\t\t<a href='?{$tag['name']}' title='{$tag['number']} plugins' rel='tag' style='font-size: {$tag['pointsize']}pt;'>{$tag['name']}</a>\n";
+		echo "\t\t\t<a href='$url&tag={$tag['name']}' title='{$tag['number']} plugins' rel='tag' style='font-size: {$tag['pointsize']}pt;'>{$tag['name']}</a>\n";
 ?>
 		</p>
-	</div>
-<?php
-} //end if( !isset($_POST['term']) ){
-?>
-	<h2>Search for Plugins<?php echo isset($_POST['term']) ? ': '.$_POST['term'] : 'via terms'; ?></h2>
-	<div class="tagsearch">
-		<form method="post" ation="<?php echo ''; ?>">
-			<input type="text" name="term" value="<?php echo $_POST['term']; ?>" />
-			<input type="submit" name="submit" value="Search" />
-		</form>
-<?php
-	if( isset($_POST['term']) ){
-		$term = $_POST['term'];
-		echo '<div id="searchresults">';
-			$results = $wpupdate->searchPlugins($term);
-			foreach((array)$results as $section=>$res){
-				if('titlematch' == $section) echo '<h2>Title Matches</h2>';
-				if('relevant' == $section) echo '<h2>Relevant Results</h2>';
-				foreach((array)$res as $plugin){
-					echo '<div class="pluginitem"><p>';
-						echo '<h3>'.$plugin['Name'].'</h3>';
-						echo nl2br($plugin['Desc']);
-						echo "<div class='section'><a href='#'>Install</a> <a href='{$plugin['Uri']}' target='_blank'>{$plugin['Name']} on WordPress.Org</a></div>\n\n";
-					echo '</p></div>';
-				}
-			}
-		echo '</div>';
-	}
-?>
 	</div>
 </div>
