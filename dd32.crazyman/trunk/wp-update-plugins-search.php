8cce272a216997cf08e0c8bc3cb9af75dc8ebb51
dd32.crazyman <dd32.crazyman@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-04 10:12:12 +0000

    Plugin Search changes.

--- dd32.crazyman/trunk/wp-update-plugins-search.php
+++ dd32.crazyman/trunk/wp-update-plugins-search.php
@@ -22,20 +22,28 @@ if( isset($_POST['term']) || isset($_GET['tag']) ){
 //var_dump($tags);
 ?>
 <style type="text/css">
-	.taglist p{
-		font-size:2em;
+	.taglist {
+		font-size:1.8em; /* overriden by the individual links */
 	}
-	.pluginitem{
-		text-indent:30px;
-		margin-bottom:0px;
+	.searchresults{
 	}
-	.pluginitem p{
-		background-color:#DEDEDE;
+	.plugin{
+		display:inline;
+		width:320px;
+		text-align:center;
+		vertical-align:top;
+	}
+	.plugin span{
+		display:inline-block;
+		border:thin solid #CCCCCC;
+		margin-bottom:20px;
+		padding:5px;
+	}
+	#load-more *{
+		border:none;
 	}
 	.pluginitem h3{
-		text-decoration:underline;
-		font-weight:bolder;
-		font-size:1.4em;
+		font-size:1em;
 	}
 	.section{
 		font-weight:bold;
@@ -44,8 +52,8 @@ if( isset($_POST['term']) || isset($_GET['tag']) ){
 	}
 </style>
 <div class="wrap">
+	<div class="searchresults">
 	<h2>Search for Plugins<?php echo !empty($searchTerm) ? ': '.$searchTerm : 'via terms'; ?></h2>
-	<div class="tagsearch">
 		<form method="post" action="<?php echo $url; ?>">
 			<input type="text" name="term" value="<?php if( !empty($searchTerm)){ echo attribute_escape($searchTerm);}  ?>" />
 			<input type="submit" name="submit" value="Search" />
@@ -53,31 +61,30 @@ if( isset($_POST['term']) || isset($_GET['tag']) ){
 <?php
 	if( ! empty($searchTerm) ){
 		echo '<div id="searchresults">';
-			$results = $wpupdate->searchPlugins($searchTerm);
-			var_dump($results);
-			foreach((array)$results as $section=>$res){
-				if('titlematch' == $section) echo '<h2>Title Matches</h2>';
-				if('relevant' == $section) echo '<h2>Relevant Results</h2>';
-				foreach((array)$res as $plugin){
-					echo '<div class="pluginitem">';
-						echo '<h3>'.$plugin['Name'].'</h3><p>';
-						echo nl2br($plugin['Desc']);
-						echo "</p><div class='section'><a href='#'>Install</a> <a href='{$plugin['Uri']}' target='_blank'>{$plugin['Name']} on WordPress.Org</a></div>\n\n";
-					echo '</div>';
+			$results = $wpupdate->search('plugins',array($searchTerm));
+			//foreach((array)$results as $section=>$res){
+				foreach((array)$results as $plugin){
+					echo '<div class="plugin"><span>';
+						echo '<h3>'.$plugin['Name'].'</h3>';
+						echo '<p>';
+							echo wordwrap($plugin['Desc'],25,"<br/>\n");
+						echo '</p>';
+						echo "<p><a href='#'>Install</a> <a href='{$plugin['Uri']}' target='_blank'>WordPress.Org</a></p>\n\n";
+					echo '</span></div> &nbsp; ';
 				} //foreach
-			} //foreach
+			//} //foreach
+			echo '<div class="plugin" id="load-more"><span><p>&nbsp;<br/><br/><br/></p><p><a href="#more" onclick="loadMore();">Load More Items</a></p></span></div>';
 		echo '</div>';
+		var_dump($results);
 	} //end if empty
 ?>
-	</div>
-
-	<h2>Search for Plugins via Tags</h2>
+	<h2>Search by Tag</h2>
 	<div class="taglist">
 		<p>
 <?php
 	$tags = $wpupdate->getPluginSearchTags();
 	foreach($tags as $tag)
-		echo "\t\t\t<a href='$url&tag={$tag['name']}' title='{$tag['number']} plugins' rel='tag' style='font-size: {$tag['pointsize']}pt;'>{$tag['name']}</a>\n";
+		echo "<a href='$url&tag={$tag['name']}' title='{$tag['number']} plugins' rel='tag' style='font-size: {$tag['pointsize']}pt;'>{$tag['name']}</a> ";
 ?>
 		</p>
 	</div>
