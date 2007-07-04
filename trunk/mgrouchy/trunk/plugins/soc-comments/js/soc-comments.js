c252dcb9ba16de4d86e812d1e7be2fddf6668bb1
mgrouchy <mgrouchy@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-04 18:13:40 +0000

    small fix to javascript so that when you hit the reply link more than once it closes the form

--- trunk/mgrouchy/trunk/plugins/soc-comments/js/soc-comments.js
+++ trunk/mgrouchy/trunk/plugins/soc-comments/js/soc-comments.js
@@ -1,23 +1,37 @@
 function addReplyForm( commentid, userident, useremail, userurl){
 	var divname = 'com-' + commentid;
 	var divid = '#' + divname;
+	
+	//check to see if the form already exists, if it does, remove it
+	if ((jQuery("*").index( jQuery('#comment-reply-form')[0] )) != -1){
+		return clearInner(divid);
+	}
+
 	// build our form and put it the comments div
 	jQuery('#' + divname).css({display: 'none'});
 	jQuery("<form action=\"/wp-comments-post.php\" method=\"POST\" id=\"comment-reply-form\">" + 
 	      "<p><textarea name=\"comment\" id=\"comment\" cols=\"100%\" rows=\"10\" tabindex=\"4\"></textarea></p>" +
 	      "<p><input name=\"submit\" type=\"submit\" id=\"submit\" tabindex=\"5\" value=\"Submit Comment\" />" +
-	      "<input type=\"hidden\" name=\"comment_post_ID\" value=\"" + commentid + "\" />" +
+	      "<input name=\"submit\" type=\"submit\" id=\"cancel\" tabindex=\"6\" value=\"Cancel\" onclick=\"return clearInner('" + divid + "')\" />" +
+		  "<input type=\"hidden\" name=\"comment_post_ID\" value=\"" + commentid + "\" />" +
 	      "<input type=\"hidden\" id=\"author\" name=\"author\" value=\"" + userident + "\" />" +
 	      "<input type=\"hidden\" id=\"email\" name=\"email\" value=\"" + useremail + "\" />" +
 	      "<input type=\"hidden\" id=\"url\" name=\"url\" value=\"" + userurl + "\" />" +
 	      "</p></form>").appendTo(divid);
 	
+	//bind our form, then show it
 	bindForm(divname,'comment-reply-form');
 	jQuery(divid).show("slow");
 	
 	return false;	
 }
 
+//function to empty an element gracefully
+function clearInner(elemid) {
+	jQuery(elemid).hide('slow');
+	jQuery(elemid).empty();
+	return false;
+}
 function bindForm(divname,formname) {
 	jQuery(document).ready(function() {
 	    var options = {
