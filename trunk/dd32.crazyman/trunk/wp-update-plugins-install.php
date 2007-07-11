228a541d6ffb2f4d19c2fd7c42517162325236ef
dd32.crazyman <dd32.crazyman@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-11 12:47:51 +0000

    PluginSearch / PluginInstall integration with Requirements checking.

--- trunk/dd32.crazyman/trunk/wp-update-plugins-install.php
+++ trunk/dd32.crazyman/trunk/wp-update-plugins-install.php
@@ -13,6 +13,63 @@ $wpupdate = new WP_Update;
 </style>
 <div class="wrap">
 	<h2>Install a Plugin</h2>
+	<?php
+		//First we check to see if this is a WordPress.org plugin, We'll have some requirements to check first :)
+		if ( isset($_GET['wp-id']) && !empty($_GET['wp-id']) && !isset($_GET['proceed']) ) {
+			$id = $_GET['wp-id'];
+			$pluginInfo = $wpupdate->checkPluginUpdateWordpressOrg($id);
+			?>
+				<p>
+					<?php _e('Are you sure you wish to install the following Plugin?'); ?>
+				</p>
+					<?php
+						$pluginCompatible = $wpupdate->checkPluginCompatible($pluginInfo);
+						if( $pluginCompatible['Compatible'] ){
+							echo '<div class="updated"><p>'. __('Plugin is compatible') . '</p></div>';
+						} else {
+							echo '<div class="error"><p>'. __('Plugin is not compatible') . '</p></div>';
+						}
+						if( isset($pluginCompatible['Errors']) ){
+							echo '<div class="error"><p>';
+							foreach( (array) $pluginCompatible['Errors'] as $message) {
+								echo $message . "<br />";
+							}
+							echo '</p></div>';
+						}
+					?>
+				<p>
+					<strong>Plugin Name: </strong><?php echo $pluginInfo['Name'] . ' ' . $pluginInfo['Version']; ?><br />
+					<strong>Author:</strong> <?php echo empty($pluginInfo['AuthorHome']) ? 
+														$pluginInfo['Author'] : 
+														'<a target="_blank" href="' . $pluginInfo['AuthorHome'] . '">' . $pluginInfo['Author'] . '</a>' ?><br/>
+					<strong>Last Updated:</strong> <?php echo $pluginInfo['LastUpdate']; ?><br />
+					<strong>Plugin Homepage:</strong> <?php echo '<a target="_blank" href="' . $pluginInfo['PluginHome'] . '">' . $pluginInfo['PluginHome'] . '</a>' ?><br />
+					<strong>Plugin Rating:</strong> <?php echo $pluginInfo['Rating']; ?>%<br />
+					<strong>Download:</strong> <?php echo '<a target="_blank" href="' . $pluginInfo['Download'] . '">' . $pluginInfo['Download'] . '</a>' ?><br/>
+					<strong>Tags:</strong><?php
+												foreach( (array) $pluginInfo['Tags'] as $tag){
+													echo '<a href="plugins.php?page=wp-update/wp-update-plugins-search.php&tag=' . $tag . '">' . 
+															str_replace('-',' ',$tag) . '</a> ';
+												}
+												?><br />
+					<strong>Related Plugins:</strong><?php
+												foreach( (array) $pluginInfo['Related'] as $related){
+													echo '<a href="plugins.php?page=wp-update%2Fwp-update-plugins-install.php&wp-id=' . $related . '">' . 
+														str_replace('-',' ',$related) . '</a> ';
+												}
+												?><br/>
+				</p>
+				<p class="submit">
+				<form action="<?php echo $pagenow ?>" method="GET">
+					<input type="hidden" name="page" value="<?php echo attribute_escape($_GET['page']); ?>" />
+					<input type="hidden" name="url" value="<?php echo attribute_escape($pluginInfo['Download']); ?>" />
+					<input type="hidden" name="proceed" value="y" />
+					<input type="submit" name="submit" value="<?php _e('Ok, Install the plugin &raquo;') ?>" />
+				</form>
+				</p>
+			<?php
+		}//end if wp-id
+	?>
 	<?php if( (isset($_GET['url']) && !empty($_GET['url']) ) || !empty($_FILES) ){
 		if( isset($_GET['url']) ){
 			//Download file
@@ -43,7 +100,7 @@ $wpupdate = new WP_Update;
 			?>
 		</p>
 	<?php } ?>
-	<?php if( (!isset($_GET['url']) || empty($_GET['url']) ) && empty($_FILES) ){ ?>
+	<?php if( (!isset($_GET['url']) || empty($_GET['url']) ) && empty($_FILES) && (!isset($_GET['wp-id']) || empty($_GET['wp-id'])) ){ ?>
 		<h3>Upload file</h3>
 		<p>
 			<form enctype="multipart/form-data" name="installlocalfile" method="POST">
