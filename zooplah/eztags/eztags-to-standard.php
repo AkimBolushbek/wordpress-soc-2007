bbff6685810c941f1bbf011e4597b7e67fd12eaf
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-14 02:13:19 +0000

    Documentation fixes; more tags implemented

--- zooplah/eztags/eztags-to-standard.php
+++ zooplah/eztags/eztags-to-standard.php
@@ -9,7 +9,10 @@ function eztags_to_translatable(&$content)
 function eztags_parse_ez(&$content)
 {
 	$content = str_replace('<$EntryTitle lang="en"$>', '<?php the_title(); ?>', $content);
+
 	$content = str_replace('<$EntryAuthor$>', '<?php the_author(); ?>', $content);
+	$content = str_replace('<$EntryId$>', '<?php the_ID(); ?>', $content);
+	$content = str_replace('<$EntryPermalink$>', '<?php the_permalink(); ?>', $content);
 
 	eztags_to_translatable($content);
 }
