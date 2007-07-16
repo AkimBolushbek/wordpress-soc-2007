5768e27de7256397f788919590d064a392c3c8e6
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-16 21:47:13 +0000

    Implemented comment_ID();

--- trunk/zooplah/eztags/eztags-from-standard.php
+++ trunk/zooplah/eztags/eztags-from-standard.php
@@ -1,13 +1,23 @@
 <?php
 
+/* Converts standard tags to easy tags.
+ * Contains functions for the conversion; format:
+ * function eztags_from_tag_name(&$ct)
+ */
+
 function eztags_from_author(&$ct)
 {
 	$ct = preg_replace('/the_author\(\s*\);?/', '?&gt;<$EntryAuthor$>&lt;?php', $ct);
 }
 
+function eztags_from_comment_id(&$ct)
+{
+	$ct = preg_replace('/comment_ID\(\s*\);?/', '?&gt;<$CommentID$>&lt;?php', $ct);
+}
+
 function eztags_from_e(&$ct)
 {
-	preg_match('/_e\(([^\)]+)\);?/', $ct, $matches);
+	preg_match('/_e\((.+)\)\;?/', $ct, $matches);
 	list($match, $content) = $matches;
 	$content = preg_replace('/\'|&quot;/', '', $content);
 
@@ -39,6 +49,7 @@ function eztags_from_title(&$ct)
 function eztags_parse_from(&$ct)
 {
 	eztags_from_author($ct);
+	eztags_from_comment_id($ct);
 	eztags_from_e($ct);
 	eztags_from_id($ct);
 	eztags_from_permalink($ct);
