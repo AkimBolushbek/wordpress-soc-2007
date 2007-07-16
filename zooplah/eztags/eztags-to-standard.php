aa0c018b3c18f85436e7ea8c7de3c13cf0a8b4d2
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-16 20:41:46 +0000

    Consistent use of capitalization in ID; more documentation clarifications

--- zooplah/eztags/eztags-to-standard.php
+++ zooplah/eztags/eztags-to-standard.php
@@ -11,7 +11,7 @@ function eztags_parse_ez(&$content)
 	$content = str_replace('<$EntryTitle lang="en"$>', '<?php the_title(); ?>', $content);
 
 	$content = str_replace('<$EntryAuthor$>', '<?php the_author(); ?>', $content);
-	$content = str_replace('<$EntryId$>', '<?php the_ID(); ?>', $content);
+	$content = str_replace('<$EntryID$>', '<?php the_ID(); ?>', $content);
 	$content = str_replace('<$EntryPermalink$>', '<?php the_permalink(); ?>', $content);
 
 	eztags_to_translatable($content);
