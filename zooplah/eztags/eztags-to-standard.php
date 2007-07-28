8c05fcdcfbd3ea96c3df98eacf5ae61453153e56
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-07-28 10:53:03 +0000

    Implemented the parameterless <$EntryTime$>

--- zooplah/eztags/eztags-to-standard.php
+++ zooplah/eztags/eztags-to-standard.php
@@ -1,19 +1,5 @@
 <?php
 
-function eztags_to_time(&$content)
-{
-	/* TODO: Get this thing working */
-	preg_match_all('/<\$EntryTime([^\$]*)\$>/', $str, $matches);
-
-	$attrs = $matches[1][0];
-
-	preg_match('/format="(\S*)"/', $attrs, $matches2);
-
-	list($match, $format) = $matches2;
-	
-	$content = str_replace($matches[0][0], "<?php the_time($format); ?>", $content);
-}
-
 function eztags_to_translatable(&$content)
 {
 	$content = str_replace('<TranslatableString>', '<?php _e(\'', $content);
@@ -31,10 +17,10 @@ function eztags_parse_ez(&$content)
 	$content = str_replace('<$EntryAuthor$>', '<?php the_author(); ?>', $content);
 	$content = str_replace('<$EntryID$>', '<?php the_ID(); ?>', $content);
 	$content = str_replace('<$EntryPermalink$>', '<?php the_permalink(); ?>', $content);
+	$content = str_replace('<$EntryTime$>', '<?php the_time(); ?>', $content);
 	$content = str_replace('<$LanguageAttributes$>', '<?php language_attributes(); ?>', $content);
 	$content = str_replace('<$WPLoginOut$>', '<?php wp_loginout(); ?>', $content);
 
-	//eztags_to_time($content);
 	eztags_to_translatable($content);
 }
 
