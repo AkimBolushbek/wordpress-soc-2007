30aeb894609bc3e2ad4d40f9b310a9bdc82e1a7b
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-08-09 21:48:22 +0000

    More tags implemented

--- zooplah/eztags/eztags-to-standard.php
+++ zooplah/eztags/eztags-to-standard.php
@@ -36,10 +36,17 @@ function eztags_parse_ez(&$content)
 	$content = str_replace('<$WPCalendar3$>', '<?php get_calendar(false); ?>', $content);
 	$content = preg_replace('/<\$WPCategories:([^\$]+)\$>/', '<?php wp_list_categories(\'$1\'); ?>', $content);
 	$content = preg_replace('/<\$WPCategoriesOld:([^\$]+)\$>/', '<?php wp_list_cats(\'$1\'); ?>', $content);
+	$content = str_replace('<$WPElse$>', '<?php else : ?>', $content);
+	$content = str_replace('<$WPEndIf$>', '<?php endif; ?>', $content);
+	$content = str_replace('<$WPEndLoop$>', '<?php endwhile; ?>', $content);
+	$content = str_replace('<$WPEntriesLoop$>', '<?php while (have_posts()) : the_post(); ?>', $content);
+	$content = str_replace('<$WPIfEntries$>', '<?php if ( have_posts() ) : ?>', $content);
 	$content = preg_replace('/<\$WPInfo:([^\$]+)\$>/', '<?php bloginfo(\'$1\'); ?>', $content);
 	$content = str_replace('<$WPLanguageAttributes$>', '<?php language_attributes(); ?>', $content);
 	$content = preg_replace('/<\$WPLinkPages:([^\$]*)\$>/', '<?php wp_link_pages(\'$1\'); ?>', $content);
 	$content = preg_replace('/<\$WPLinks:([^\$]+)\$>/', '<?php get_links_list(\'$1\'); ?>', $content);
+	$content = str_replace('<$WPLoadHeader$>', '<?php get_header(); ?>', $content);
+	$content = str_replace('<$WPLoadSidebar$>', '<?php get_sidebar(); ?>', $content);
 	$content = str_replace('<$WPLoginOut$>', '<?php wp_loginout(); ?>', $content);
 	$content = str_replace('<$WPMeta$>', '<?php wp_meta(); ?>', $content);
 	$content = preg_replace('/<\$WPPages:([^\$]*)\$>/', '<?php wp_list_pages(\'$1\'); ?>', $content);
