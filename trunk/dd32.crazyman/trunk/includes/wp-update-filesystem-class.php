e9213ec18c5dd81b945efc3d35d280e500516e2c
dd32.crazyman <dd32.crazyman@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-08-09 09:15:31 +0000

    global $wp_filesystem instead of function returning the FS object

--- trunk/dd32.crazyman/trunk/includes/wp-update-filesystem-class.php
+++ trunk/dd32.crazyman/trunk/includes/wp-update-filesystem-class.php
@@ -1,35 +1,30 @@
 <?php
 function WP_Filesystem_check($preference=false,$arg=false){
-	return WP_Filesystem($preference,$arg,'check');
+	global $wp_filesystem;
+	if( ! $wp_filesystem )
+		WP_Filesystem($preference,$arg);
+	return empty($wp_filesystem->errors);
 }
-function WP_Filesystem($preference=false,$arg=false,$action='connect'){
+function WP_Filesystem($preference=false,$arg=false){
+	global $wp_filesystem;
 	if( ! $preference )
 		$preference = get_option('wpfs_method');
 
 	$method = _WP_Filesystem_bestOption($preference);
-	if( ! $method ) return;
+	if( ! $method )
+		return false;
 	if( strpos($method,'ftp') > -1 )
 		$arg = array_merge((array)get_option('wpfs_ftp'), (array)$arg);
 
 	require_once('wp-update-filesystem-'.$method.'-class.php');
 	$method = "WP_Filesystem_$method";
-	$fs = new $method($arg);
-	if( 'check' == $action ){
-		$ret = $fs->errors;
-		unset($fs);
-		return !empty($ret) ? $ret : true;
-	}
-	if ('connect' == $action){
-		$numerrors = count($fs->errors);
-		$fs->connect();
-		if( $numerrors != count($fs->errors) ){
-			//There was an erorr connecting to the server.
-			$ret = $fs->errors;
-			unset($fs);
-			return $ret;
-		}
-	}
-	return $fs;
+	$wp_filesystem = new $method($arg);
+
+	$numerrors = count($wp_filesystem->errors);
+	$wp_filesystem->connect();
+	if( $numerrors != count($wp_filesystem->errors) )
+		return false; //There was an erorr connecting to the server.
+	return true;
 }
 function _WP_Filesystem_bestOption($preference='direct'){
 	switch($preference){
