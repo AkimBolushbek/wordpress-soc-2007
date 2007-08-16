22bd132c26038676df79b6e99e73c507bd17b20a
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-08-16 02:51:18 +0000

    Added documentation for the public eztags_bind() function

--- zooplah/eztags/eztags-functions-public.php
+++ zooplah/eztags/eztags-functions-public.php
@@ -1,6 +1,11 @@
 <?php
 
-function eztags_bind($func, $param = array())
+/**
+ @desc Binds a function as a conversion routine
+ @param string A function to bind
+ @param array List of arguments to send to the function
+*/
+function eztags_bind($func, $param = NULL)
 {
 	global $_eztags_bind_array;
 	$_eztags_bind_array[$func] = array($func, $param);
