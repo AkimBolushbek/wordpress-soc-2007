21b18b250c7464c5f69208f11325d5f430ba8816
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-08-18 01:30:15 +0000

    Links to the documentation from the side panel actually works now; updated the docs to include support for attributes

--- zooplah/eztags/includes/eztags-functions-private.php
+++ zooplah/eztags/includes/eztags-functions-private.php
@@ -7,12 +7,8 @@ function get_eztags_dir()
 	list($match, $relative_file) = $matches;
 	$eztags_dir = dirname($relative_file);
 
-	if ( '.' != $eztags_dir )
+	if ( $eztags_dir )
 		$eztags_dir .= '/';
-	/* $eztags_dir is only a point;
-	 * that is, not in a subdirectory */
-	else
-		$eztags_dir = '';
 
 	return $eztags_dir;
 }
