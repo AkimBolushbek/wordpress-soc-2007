7c3ab764e6c20c057bfab6262b81de7a6f7666ec
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-08-20 04:34:17 +0000

    Creating a snapshot of the project as it existed at the end of Summer of Code 2007; it's the same as the to-be-submitted tarball snapshot, except it's unmade

---
+++ tags/eztags-gsoc2007-final/eztags/eztags.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: Easier Template Tags
+Plugin URI: http://wordpress-soc-2007.googlecode.com/svn/trunk/zooplah/
+Description: Provides an editor that allows you to easily edit themes without seeing much of the underlying PHP code.
+Version: <a href="http://en.wikipedia.org/wiki/Renard_(James_Bond)">Renard</a>
+Author: Keith Bowes
+Author URI: http://zooplah.farvista.net/
+*/
+
+/*  Copyright 2007  Easier Template Tags
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+require_once 'includes/eztags-functions-public.php';
+
+function add_eztags_admin_pages()
+{
+	add_theme_page(_z('Easier Template Tags'), _z('Easier Theme Editor'), edit_themes, get_eztags_dir() . 'pages/eztags-subpanel.php');
+}
+
+add_action('admin_menu', 'add_eztags_admin_pages');
+
+?>
---
+++ tags/eztags-gsoc2007-final/eztags/includes/eztags-functions-private.php
@@ -0,0 +1,56 @@
+<?php
+
+/* Get the relative path where the plugin is installed */
+function get_eztags_dir()
+{
+	preg_match('/\/wp-content\/plugins\/(.*)$/', htmlspecialchars(__FILE__), $matches);
+	list($match, $relative_file) = $matches;
+	$eztags_dir = dirname($relative_file);
+
+	if ( $eztags_dir )
+		$eztags_dir .= '/';
+
+	return $eztags_dir;
+}
+
+/* Set up stuff for internationalization */
+$eztags_domain = 'eztags';
+load_plugin_textdomain($eztags_domain, 'wp-content/plugins/' . get_eztags_dir() . 'languages');
+
+function _ez($str)
+{
+	global $eztags_domain;
+	_e($str, $eztags_domain);
+}
+
+function _z($str)
+{
+	global $eztags_domain;
+	return __($str, $eztags_domain);
+}
+
+/* Set up for binding custom tags */
+$_eztags_bind_array = array();
+
+function eztags_wind(&$content)
+{
+	global $_eztags_bind_array;
+
+	foreach ($_eztags_bind_array as $binded)
+	{
+		list($func, $param) = $binded;
+
+		if ( NULL !== $param )
+		{
+			if ( !is_array($param) )
+				$param = array($param);
+
+			array_unshift($param, &$content);
+			call_user_func_array($func, $param);
+		}
+		else
+			call_user_func($func, &$content);
+	}
+}
+
+?>
