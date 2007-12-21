2ec9a13e1881a6ca5f5d173f5edc323879023fe8
zooplah <zooplah@e5d9faa3-ef2d-0410-a253-4bf142219f3e>
2007-12-21 03:16:47 +0000

    Adding Renard 0.1 to svn as tag

---
+++ zooplah/tags/renard-0.1/renard/includes/renard-mediator.php
@@ -0,0 +1,42 @@
+<?php
+
+/* Mediates between the two types of template tags */
+
+require_once 'renard-from-standard.php';
+require_once 'renard-to-standard.php';
+
+function remove_empty_php(&$content)
+{
+	$content = preg_replace('/&lt;\?php\s*\?&gt;/', '', $content);
+}
+
+function remove_extra_lines(&$content)
+{
+	$content = preg_replace('/[\n\r]{2,}/m', "\n\n", $content);
+}
+
+/* Replace Standard tags with Easy tags */
+function std2ez($content)
+{
+	renard_parse_std($content);
+	remove_empty_php($content);
+	remove_extra_lines($content);
+
+	return $content;
+}
+
+/* Replace Easy tags with Standard tags */
+function ez2std($content)
+{
+	renard_parse_ez($content);
+	// Quitting and then instantly restarting PHP mode con cause
+	// problems
+	$content = preg_replace('/(\s*)\?><\?php\s*/', '$1', $content);
+
+	// Localized empty string is not the same as empty
+	$content = str_replace("__('')", '', $content);
+
+	return $content;
+}
+
+?>
